e10debdacd889b4f9734aae7d6ae40cca7988a96
Anita <Efosa@Anita-Efosa.local>
2022-06-22 16:31:18 +0100

    blogcard component created

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,14 +2,23 @@
   <div class="home">
     <BlogPost :post="welcomeScreen"/>
     <BlogPost :post="post" v-for="(post, index) in sampleBlogPost" :key="index"/>
+    <div class="blog-card-wrap">
+      <div class="container">
+        <h3>View More Recent Blogs</h3>
+        <div class="blog-cards">
+          <BlogCards v-for="(post, index) in sampleBlogCards" :key="index"/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import BlogPost from "../components/BlogPost.vue";
+import BlogCards from "../components/BlogCard.vue";
 export default {
   name: "Home",
-  components: { BlogPost },
+  components: { BlogPost, BlogCards },
   data() {
     return {
       welcomeScreen: {
@@ -30,7 +39,16 @@ export default {
           blogCoverPhoto: "designed-for-everyone",
         },
       ],
+      sampleBlogCards: [
+        {blogTitle: "Blog Card #1", blogCoverPhoto: "stock-1", blogDate: "May 1, 2021"},
+        {blogTitle: "Blog Card #2", blogCoverPhoto: "stock-2", blogDate: "May 1, 2021"},
+        {blogTitle: "Blog Card #3", blogCoverPhoto: "stock-3", blogDate: "May 1, 2021"},
+        {blogTitle: "Blog Card #4", blogCoverPhoto: "stock-4", blogDate: "May 1, 2021"},
+      ]
     };
   },
 };
 </script>
+<style lang="scss" scoped>
+
+</style>
