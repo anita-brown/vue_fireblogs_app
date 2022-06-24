c9561e69d570e3d51f844d42b051255f98957d70
Anita <Efosa@Anita-Efosa.local>
2022-06-24 12:27:53 +0100

    manageing states for navigation

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="app-wrapper">
     <div class="app">
-      <Navigation />
+      <Navigation v-if="!navigation"/>
       <router-view />
-      <Footer />
+      <Footer v-if="!navigation"/>
     </div>
   </div>
 </template>
@@ -15,12 +15,28 @@ export default {
   name: "app",
   components: { Navigation, Footer },
   data() {
-    return {};
+    return {
+      navigation: null,
+    };
   },
-  created() {},
+  created() {
+    this.checkRoute();
+  },
   mounted() {},
-  methods: {},
-  watch: {},
+  methods: {
+    checkRoute() {
+      if (this.$route.name === "Login" || this.$route.name === "Register" || this.$route.name === "ForgotPassword") {
+        this.navigation = true;
+        return;
+      }
+      this.navigation = false;
+    },
+  },
+  watch: {
+    $route() {
+      this.checkRoute();
+    }
+  },
 };
 </script>
 
@@ -106,8 +122,7 @@ button,
   background-color: transparent;
   border: 2px solid #fff;
   color: #fff;
-
-} 
+}
 .button-inactive {
   pointer-events: none !important;
   cursor: none !important;
