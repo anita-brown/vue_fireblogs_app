0364c10ce7abaa1e9829d7621f325c250ac8a930
Anita <Efosa@Anita-Efosa.local>
2022-06-24 15:03:17 +0100

    update navigation

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="reset-password">
+    <Modal v-if="modalActive" v-on:close-modal="closeModal"/>
+    <Loading v-if="loading"/>
     <div class="form-wrap">
       <form class="reset">
         <h2>Reset Password</h2>
@@ -20,11 +22,30 @@
 
 <script>
 import email from "../assets/Icons/envelope-regular.svg";
+import Loading from '../components/Loading.vue';
+import Modal from "../components/Modal.vue";
+
 export default {
   name: "ForgotPassword",
+  data() {
+    return {
+      email: null,
+      modalActive: false,
+      modalMessage: "",
+      loading: null,
+    };
+  },
   components: {
     email,
+    Modal,
+    Loading,
   },
+  methods: {
+      closeModal() {
+          this.modalActive = !this.modalActive;
+          this.email = "";
+      }
+  }
 };
 </script>
 
